3b0562c332390cc2b99ee15820648b269536c536
icaroagostino <icaroagostino@hotmail.com>
2020-08-06 12:45:25 +0200

    fisrt commit

---
+++ run_analysis.R
@@ -0,0 +1,8 @@
+
+# Download data
+download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip',
+              destfile = 'UCI_HAR_Dataset.zip')
+
+# Unzip
+unzip('UCI_HAR_Dataset.zip')
+
